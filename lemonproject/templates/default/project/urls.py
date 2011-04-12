9bac142705459d56c89e375b2863af5112fbcc8f
Dima Kukushkin <kukushkin@trilan.ru>
2011-04-12 22:15:54 +0400

    Fix serving static in debug mode

--- lemonproject/templates/default/project/urls.py
+++ lemonproject/templates/default/project/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
 from lemon import extradmin
@@ -19,4 +20,11 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         url(r'^404$', TemplateView.as_view(template_name='404.html')),
         url(r'^500$', TemplateView.as_view(template_name='500.html')),
+    )
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns(
+        '',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
     )
